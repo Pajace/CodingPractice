b440895bb506799d1795c5d11dc7b868052c3aa2
pajace_chen <pajace_chen@htc.com>
2014-08-27 20:45:55 +0800

    Solve Problem - SameTree

---
+++ Leecodeoj.unititest/SameTree_Unittest.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leecodeoj;
+
+namespace Leecodeoj.unititest
+{
+    [TestClass]
+    public class SameTree_Unittest
+    {
+        private void assertIsTheSameTree(TreeNode root1, TreeNode root2,
+                    bool expected)
+        {
+            SameTree st = new SameTree();
+            bool actual = st.isSameTree(root1, root2);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SameTree_isSameTree_SameTree()
+        {
+            TreeNode root1 = createTreeNode9NodeType1_value1();
+            TreeNode root2 = createTreeNode9NodeType1_value1();
+
+            bool expected = true;
+            assertIsTheSameTree(root1, root2, expected);
+        }
+
+        [TestMethod]
+        public void SameTree_isSameTree_SameStructureWithDifferentValue()
+        {
+            TreeNode root1 = createTreeNode9NodeType1_value1();
+            TreeNode root2 = createTreeNode9NodeType1_value2();
+
+            bool expected = false;
+            assertIsTheSameTree(root1, root2, expected);
+        }
+
+        [TestMethod]
+        public void SameTree_isSameTree_NodeWithNull()
+        {
+            TreeNode root1 = createTreeNode9NodeType1_value1();
+
+            bool expected = false;
+            assertIsTheSameTree(root1, null, expected);
+        }
+
+        [TestMethod]
+        public void SameTree_isSameTree_NullWithNode()
+        {
+            TreeNode root1 = createTreeNode9NodeType1_value1();
+
+            bool expected = false;
+            assertIsTheSameTree(null, root1, expected);
+        }
+
+        [TestMethod]
+        public void SameTree_isSameTree_BothNull()
+        {
+            assertIsTheSameTree(null, null, true);
+        }
+
+        //       1
+        //      / \
+        //     2    3
+        //    / \   /\
+        //   4   5 6  7
+        //  / \
+        // 8   9
+        private TreeNode createTreeNode9NodeType1_value1()
+        {
+            TreeNode node1 = new TreeNode(1);
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            TreeNode node4 = new TreeNode(4);
+            TreeNode node5 = new TreeNode(5);
+            TreeNode node6 = new TreeNode(6);
+            TreeNode node7 = new TreeNode(7);
+            TreeNode node8 = new TreeNode(8);
+            TreeNode node9 = new TreeNode(9);
+
+            node1.left = node2;
+            node1.right = node3;
+            node2.left = node4;
+            node2.right = node5;
+            node3.left = node6;
+            node3.right = node7;
+            node4.left = node8;
+            node7.right = node9;
+
+            return node1;
+        }
+
+        //       1
+        //      / \
+        //     2   5
+        //    / \  /\
+        //   3  4 6  7
+        //  / \
+        // 8   9
+        private TreeNode createTreeNode9NodeType1_value2()
+        {
+            TreeNode node1 = new TreeNode(1);
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            TreeNode node4 = new TreeNode(4);
+            TreeNode node5 = new TreeNode(5);
+            TreeNode node6 = new TreeNode(6);
+            TreeNode node7 = new TreeNode(7);
+            TreeNode node8 = new TreeNode(8);
+            TreeNode node9 = new TreeNode(9);
+
+            node1.left = node2;
+            node1.right = node5;
+            node2.left = node3;
+            node2.right = node4;
+            node3.left = node8;
+            node5.left = node6;
+            node5.right = node7;
+            node7.right = node9;
+
+            return node1;
+        }
+    }
+}
---
+++ Leecodeoj/SameTree.cs
@@ -0,0 +1,63 @@
+﻿//-----------------------------------------------------------------------
+// <author>Pajace Chen</author>
+// <email>pajace@gmail.com</email>
+// <Reference>https://oj.leetcode.com/problems/same-tree/</reference>
+//----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Leecodeoj
+{
+    /// <summary>
+    /// Given two binary trees, write a function to check if they are equal or not.
+    /// Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+    /// </summary>
+    public class SameTree
+    {
+        public bool isSameTree(TreeNode root1, TreeNode root2)
+        {
+            if (root1 == null && root2 == null) return true;
+            if (root1 == null ^ root2 == null) return false;// bug here
+
+            Stack<TreeNode> stack1 = new Stack<TreeNode>();
+            Stack<TreeNode> stack2 = new Stack<TreeNode>();
+
+            stack1.Push(root1);
+            stack2.Push(root2);
+
+            while (stack1.Count > 0)
+            {
+                var node1 = stack1.Pop();
+                var node2 = stack2.Pop();
+
+                if (node1.val != node2.val)
+                    return false;
+
+                if (node1.left != null && node2.left != null)
+                {
+                    stack1.Push(node1.left);
+                    stack2.Push(node2.left);
+                }
+                else if (node1.left == null ^ node2.left == null)
+                {
+                    return false;
+                }
+
+                if (node1.right != null && node2.right != null)
+                {
+                    stack1.Push(node1.right);
+                    stack2.Push(node2.right);
+                }
+                else if (node1.right == null ^ node2.right == null)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
