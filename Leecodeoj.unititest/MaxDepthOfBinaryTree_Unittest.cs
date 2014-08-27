f0a05485627f61407e7589722a553aee68ead35a
pajace_chen <pajace_chen@htc.com>
2014-08-27 17:52:32 +0800

    Add problem - MaxDepthOfBinaryTree

---
+++ Leecodeoj.unititest/MaxDepthOfBinaryTree_Unittest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Leecodeoj.unititest
+{
+    [TestClass]
+    public class MaxDepthOfBinaryTree_Unittest
+    {
+        private void AssertMaxDepth(TreeNode root, int expectedDepth)
+        {
+            MaxDepthOfBinaryTree maxDepthOfBinaryTree = new MaxDepthOfBinaryTree();
+            int actualDepth = maxDepthOfBinaryTree.CalculateMaxDepth(root);
+
+            Assert.AreEqual(expectedDepth, actualDepth);
+        }
+
+        [TestMethod]
+        public void MaxDepthOfBinaryTree_CalculateMaxDepth_0Depth()
+        {
+            AssertMaxDepth(null, 0);
+        }
+    }
+}
---
+++ Leecodeoj/MaxDepthOfBinaryTree.cs
@@ -0,0 +1,45 @@
+﻿//-----------------------------------------------------------------------
+// <author>Pajace Chen</author>
+// <email>pajace@gmail.com</email>
+// <Reference>https://oj.leetcode.com/problems/maximum-depth-of-binary-tree/</reference>
+//----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leecodeoj
+{
+    
+    /// <summary>
+    /// Definition for binary tree
+    /// public class TreeNode {
+    ///     int val;
+    ///     TreeNode left;
+    ///     TreeNode right;
+    ///     TreeNode(int x) { val = x; }
+    /// }
+    /// </summary>
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { this.val = x; }
+    }
+
+    /// <summary>
+    /// Given a binary tree, find its maximum depth.    
+    /// </summary>
+    public class MaxDepthOfBinaryTree
+    {
+        public int CalculateMaxDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+            return -1; // Not Implement
+        }
+    }
+}
