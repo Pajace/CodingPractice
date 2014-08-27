07a9fea310af8a90d823eccaf278cbb63a3d4368
pajace_chen <pajace_chen@htc.com>
2014-08-27 22:25:14 +0800

    Solve problem - ReverseInteger.

---
+++ Leecodeoj.unititest/ReverseInteger_Unittest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leecodeoj;
+
+namespace Leecodeoj.unititest
+{
+    [TestClass]
+    public class ReverseInteger_Unittest
+    {
+        private void AssertReverseInteger(int inputNumber, int expected)
+        {
+            ReverseInteger r = new ReverseInteger();
+            int actual = r.reverse_solution2(inputNumber);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ReverseInteger_reverse_case1()
+        {
+            int expected = 321;
+            int inputNumber = 123;
+
+            AssertReverseInteger(inputNumber, expected);
+        }
+
+        [TestMethod]
+        public void ReverseInteger_reverse_case2()
+        {
+            int expected = -321;
+            int inputNumber = -123;
+
+            AssertReverseInteger(inputNumber, expected);
+        }
+
+        [TestMethod]
+        public void ReverseInteger_reverse_case3()
+        {
+            int expected = -789456123;
+            int inputNumber = -321654987;
+
+            AssertReverseInteger(inputNumber, expected);
+        }
+
+        [TestMethod]
+        public void ReverseInteger_reverse_case4()
+        {
+            int expected = 987654321;
+            int inputNumber = 1234567890;
+
+            AssertReverseInteger(inputNumber, expected);
+        }
+
+        [TestMethod]
+        public void ReverseInteger_reverse_case5()
+        {
+            int expected = 6540321;
+            int inputNumber = 1230456;
+
+            AssertReverseInteger(inputNumber, expected);
+        }
+
+        [TestMethod]
+        public void ReverseInteger_reverse_case6()
+        {
+            int expected = 1;
+            int inputNumber = 1000000000;
+
+            AssertReverseInteger(inputNumber, expected);
+        }
+    }
+}
---
+++ Leecodeoj/ReverseInteger.cs
@@ -0,0 +1,35 @@
+﻿//-----------------------------------------------------------------------
+// <author>Pajace Chen</author>
+// <email>pajace@gmail.com</email>
+// <Reference>https://oj.leetcode.com/problems/reverse-integer/</reference>
+//----------------------------------------------------------------------
+
+namespace Leecodeoj
+{
+    public class ReverseInteger
+    {
+        public int reverse_solution2(int inputNumber)
+        {
+            bool isNegative = false;
+            if (inputNumber < 0) { 
+                isNegative = true;
+                inputNumber *= -1;
+            }
+
+            int result = 0;
+            while (true)
+            {
+                int digit = inputNumber % 10;
+                result = result * 10 + digit;
+                inputNumber /= 10;
+                if (inputNumber == 0)
+                    break;
+            }
+
+            if (isNegative)
+                result *= -1;
+
+            return result;
+        }
+    }
+}
