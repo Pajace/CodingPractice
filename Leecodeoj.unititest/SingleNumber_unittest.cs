60c1b4c7e63c02f9d914a545573f30e81887ea39
pajace_chen <pajace_chen@htc.com>
2014-08-27 16:08:46 +0800

    Finish Problem - SingleNumber

---
+++ Leecodeoj.unititest/SingleNumber_unittest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leecodeoj.unititest
+{
+    [TestClass]
+    public class SingleNumber_unittest
+    {
+        private int[] data = new int[60001];
+
+        [TestInitialize]
+        public void setup()
+        {
+            for (int i = 0; i < 60000; i += 2)
+            {                
+                data[i] = i;
+                data[i + 1] = i;
+            }
+            data[60000] = -99;
+        }
+
+        [TestMethod]
+        public void SingleNumber_TestCase_XOR()
+        {
+            int actual = SingleNumber.SolutionXOR(data);
+            int expected = -99;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SingleNumber_TestCase_SortFirst()
+        {
+            int actual = SingleNumber.SolutionSortFirst(data);
+            int expected = -99;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Leecodeoj/SingleNumber.cs
@@ -0,0 +1,54 @@
+﻿//-----------------------------------------------------------------------
+// <author>Pajace Chen</author>
+// <email>pajace@gmail.com</email>
+// <Reference>https://oj.leetcode.com/problems/single-number/</reference>
+//----------------------------------------------------------------------
+
+using System;
+using System.Linq;
+
+namespace Leecodeoj
+{
+    /// <summary>
+    /// Given an array of integers, every element appears twice except for one. Find that single one.
+    /// Note:
+    /// Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+    /// </summary>
+    public static class SingleNumber
+    {
+        public static int SolutionXOR(int[] A)
+        {
+            int result = A[0];
+            for (int i = 1; i < A.Length; i++)
+            {
+                result = result ^ A[i];
+            }
+            return result;
+        }
+
+        public static int SolutionSortFirst(int[] A)
+        {
+            Array.Sort(A);
+            int result = -1;
+
+            Boolean isFirstHit = true;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (isFirstHit)
+                {
+                    result = A[i];
+                    isFirstHit = false;
+                }
+                else
+                {
+                    if (result == A[i])
+                        isFirstHit = true;
+                    else
+                        break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
