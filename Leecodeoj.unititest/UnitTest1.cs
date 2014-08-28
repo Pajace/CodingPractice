1d95a7f16c545d1c8700b536514a0f851691bd45
pajace_chen <pajace_chen@htc.com>
2014-08-28 17:36:15 +0800

    Add Problem BestTimeToBy

---
+++ Leecodeoj.unititest/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leecodeoj;
+
+namespace Leecodeoj.unititest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private void assertMaxProfit(int[] prices, int expectedMaxProfit)
+        {
+            BestTimeToBy_1 b = new BestTimeToBy_1();
+
+            int actualMaxProfit = b.maxProfit(prices);
+
+            Assert.AreEqual(expectedMaxProfit, actualMaxProfit);
+        }
+
+        [TestMethod]
+        public void BestTimeToBye_1_Proft4()
+        {
+            int[] price = { 3, 2, 6, 5, 0, 3 };
+            int expectedProfit = 4;
+
+            assertMaxProfit(price, expectedProfit);
+        }
+
+        [TestMethod]
+        public void BestTimeToBy_1_ZeroInput()
+        {
+            int expectedProfit = 0;
+            int[] prices = { };
+
+            assertMaxProfit(prices, expectedProfit);
+        }
+
+        [TestMethod]
+        public void BestTimeToBy_Profit0()
+        {
+            int expectedProfit = 0;
+            int[] prices = { 2, 1 };
+
+            assertMaxProfit(prices, expectedProfit);
+        }
+    }
+}
---
+++ Leecodeoj/BestTimeToBy_1.cs
@@ -0,0 +1,25 @@
+﻿//-----------------------------------------------------------------------
+// <author>Pajace Chen</author>
+// <email>pajace@gmail.com</email>
+// <Reference>https://oj.leetcode.com/problems/best-time-to-buy-and-sell-stock/</reference>
+//----------------------------------------------------------------------
+
+// Problem:
+// Say you have an array for which the i-th element is the price of a given stock on day i.
+// If you were only permitted to complete at most one transaction (i.e, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+using System;
+namespace Leecodeoj
+{
+    public class BestTimeToBy_1
+    {
+        /// <summary>
+        /// get the maxProfit at most one transaction
+        /// </summary>
+        /// <param name="prices">Input prices</param>
+        /// <returns>maxProfit</returns>
+        public int maxProfit(int[] prices)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
