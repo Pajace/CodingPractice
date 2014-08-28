4131e2a884f59c33b5e2c7ba729876277c38fa6e
pajace_chen <pajace_chen@htc.com>
2014-08-28 11:28:03 +0800

    Solve problem - RemoveDuplication

---
+++ Leecodeoj.unititest/RemoteDuplicate_unittest.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Leecodeoj.unititest
+{
+    [TestClass]
+    public class RemoteDuplicate_unittest
+    {
+        private void AssertRemoveDuplication(int[] data, int[] expectedArray)
+        {
+            RemoveDuplication r = new RemoveDuplication();
+            int actualLength = r.RemoveDuplicate(data);
+
+            int expectedLength = expectedArray.Length;
+
+            Assert.AreEqual(expectedLength, actualLength);
+
+            for (int i = 0; i < actualLength; i++)
+            {
+                Assert.AreEqual(data[i], expectedArray[i],
+                        string.Format("Expected: {0}, Actual: {1}",
+                                       GetArrayContent(expectedArray), GetArrayContent(data))
+                    );
+            }
+        }
+
+        private string GetArrayContent(int[] data)
+        {
+            string result = "[";
+            foreach (int item in data)
+            {
+                result += item.ToString() + ",";
+            }
+
+            result = result.Remove(result.Length - 1, 1) + "]";
+
+            return result;
+        }
+
+
+        [TestMethod]
+        public void testcase()
+        {
+            int[] data = { };
+            int[] expected = { };
+
+            AssertRemoveDuplication(data, expected);
+        }
+
+        [TestMethod]
+        public void testCase0()
+        {
+            int[] data = { 1 };
+            int[] expected = { 1 };
+
+            AssertRemoveDuplication(data, expected);
+        }
+
+        [TestMethod]
+        public void testCase1()
+        {
+            int[] data = { 1, 1 };
+            int[] expected = { 1 };
+
+            AssertRemoveDuplication(data, expected);
+        }
+
+        [TestMethod]
+        public void testCase2()
+        {
+            int[] data = { 1, 2 };
+            int[] expected = { 1, 2 };
+
+            AssertRemoveDuplication(data, expected);
+        }
+
+        [TestMethod]
+        public void testCase3()
+        {
+            int[] data = { 1, 1, 1 };
+            int[] expected = { 1 };
+
+            AssertRemoveDuplication(data, expected);
+        }
+
+        [TestMethod]
+        public void testCase4()
+        {
+            int[] data = { 1, 2, 2 };
+            int[] expected = { 1, 2 };
+
+            AssertRemoveDuplication(data, expected);
+        }
+
+        [TestMethod]
+        public void testCase5()
+        {
+            int[] data = { 1, 1, 2 };
+            int[] expected = { 1, 2 };
+
+            AssertRemoveDuplication(data, expected);
+        }
+
+        [TestMethod]
+        public void testCase6()
+        {
+            int[] data = { 1, 2, 3 };
+            int[] expected = { 1, 2, 3 };
+
+            AssertRemoveDuplication(data, expected);
+        }
+
+        [TestMethod]
+        public void testCase7()
+        {
+            int[] data = { 1, 1, 1, 1 };
+            int[] expected = { 1 };
+
+            AssertRemoveDuplication(data, expected);
+        }
+
+        [TestMethod]
+        public void testCase8()
+        {
+            int[] data = { 1, 1, 2, 2 };
+            int[] expected = { 1, 2 };
+
+            AssertRemoveDuplication(data, expected);
+        }
+
+        [TestMethod]
+        public void testCase9()
+        {
+            int[] data = { 1, 1, 2, 3 };
+            int[] expected = { 1, 2, 3 };
+
+            AssertRemoveDuplication(data, expected);
+        }
+    }
+}
---
+++ Leecodeoj/RemoveDuplication.cs
@@ -0,0 +1,33 @@
+﻿//-----------------------------------------------------------------------
+// <author>Pajace Chen</author>
+// <email>pajace@gmail.com</email>
+// <Reference>https://oj.leetcode.com/problems/remove-duplicates-from-sorted-array/</reference>
+//----------------------------------------------------------------------
+
+namespace Leecodeoj
+{
+    public class RemoveDuplication
+    {
+        /// <summary>
+        /// Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+        /// Do not allocate extra space for another array, you must do this in place with constant memory.
+        /// </summary>
+        /// <param name="data">sorted arrry</param>
+        /// <returns>The size of array that remove duplicate</returns>
+        public int RemoveDuplicate(int[] data)
+        {
+            if (data.Length == 0) return 0;
+
+            int index = 0;
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[index] != data[i])
+                {
+                    index++;
+                    data[index] = data[i];
+                }
+            }
+            return index+1;
+        }
+    }
+}
