57892150f0c543d15a0cafd4fb6aca0bdb6726de
pajace_chen <pajace_chen@htc.com>
2014-08-29 10:14:45 +0800

    Solve Problem: Best Time to By in Stock I

--- Leecodeoj/BestTimeToBy_1.cs
+++ Leecodeoj/BestTimeToBy_1.cs
@@ -4,7 +4,7 @@
 // <Reference>https://oj.leetcode.com/problems/best-time-to-buy-and-sell-stock/</reference>
 //----------------------------------------------------------------------
 
-// Problem:
+// Problem: Best Time to By in Stock I
 // Say you have an array for which the i-th element is the price of a given stock on day i.
 // If you were only permitted to complete at most one transaction (i.e, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
 using System;
@@ -19,7 +19,20 @@ namespace Leecodeoj
         /// <returns>maxProfit</returns>
         public int maxProfit(int[] prices)
         {
-            throw new NotImplementedException();
+            if (prices.Length <= 1) return 0;
+            int maxProfit = 0;
+
+            int minPrice = prices[0];
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (prices[i] < minPrice)
+                    minPrice = prices[i];
+                int pridictProfit = prices[i] - minPrice;
+                if (pridictProfit > maxProfit)
+                    maxProfit = pridictProfit;
+            }
+
+            return maxProfit;
         }
     }
 }
