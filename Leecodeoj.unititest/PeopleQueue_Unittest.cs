e448bfe32175d682da649de6ad185851031bafeb
pajace_chen <pajace_chen@htc.com>
2014-09-06 17:11:51 +0800

    新增一題問題從 Ptt 看到的題目, 排隊問題

---
+++ Leecodeoj.unititest/PeopleQueue_Unittest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Leecodeoj.unititest
+{
+    [TestClass]
+    public class PeopleQueue_Unittest
+    {
+        private void compareTwoArray(int[] expecteds, int[] actuals)
+        {
+            Assert.AreEqual(expecteds.Length, actuals.Length, "The array length is not equal.");
+
+            for (int i = 0; i < expecteds.Length; i++)
+            {
+                Assert.AreEqual(expecteds[i], actuals[i], "The element at index {0} is different.", i);
+            }
+        }
+
+        [TestMethod]
+        public void test()
+        {
+            int[] expecteds = { 3, 6, 9, 2, 7, 1, 8 };
+            int[] actuals = PeopleQueue.Select(10, 3, 3, 7);
+
+            compareTwoArray(expecteds, actuals);
+        }
+
+        [TestMethod]
+        public void test2()
+        {
+            int[] expecteds = { 6, 11, 4, 10, 5, 1, 9 };
+            int[] actuals = PeopleQueue.Select(12, 6, 5, 7);
+
+            compareTwoArray(expecteds, actuals);
+        }
+
+        [TestMethod]
+        public void test3()
+        {
+            int[] expecteds = { 4, 8, 2, 7, 3, 10 };
+            int[] actuals = PeopleQueue.Select(10, 4, 4, 6);
+
+            compareTwoArray(expecteds, actuals);
+        }
+
+        [TestMethod]
+        public void test4()
+        {
+            int[] expecteds = { 8, 3, 14, 11, 9, 7, 10, 13, 2, 12 };
+            int[] actuals = PeopleQueue.Select(15, 8, 10, 10);
+
+            compareTwoArray(expecteds, actuals);
+        }
+    }
+}
---
+++ Leecodeoj/PeopleQueue.cs
@@ -0,0 +1,29 @@
+﻿//-----------------------------------------------------------------------
+// <author>Pajace Chen</author>
+// <email>pajace@gmail.com</email>
+//----------------------------------------------------------------------
+
+using System;
+namespace Leecodeoj
+{
+    /// <summary>
+    /// 問題描述：
+    ///      有 totalPerson 個人在排隊，從第 startPerson 開始（含），每隔 interval 取一人，共取 countOfSelected 個人
+    ///      若遇到最後一個則接著第一個往下數
+    /// </summary>
+    public class PeopleQueue
+    {
+        /// <summary>
+        /// 排隊問題
+        /// </summary>
+        /// <param name="totalPerson">排隊總人數</param>
+        /// <param name="startPerson">開始位置</param>
+        /// <param name="interval">間隔人數</param>
+        /// <param name="countOfSelected">總共選取人數</param>
+        /// <returns></returns>
+        public static int[] Select(int totalPerson, int startPerson, int interval, int countOfSelected)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
