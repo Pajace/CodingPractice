58d87c38840776d0b7f3c3a5de4d233c4cfad50a
pajace_chen <pajace_chen@htc.com>
2014-09-10 08:51:57 +0800

    Solve problem - candy

--- Leecodeoj/Candy.cs
+++ Leecodeoj/Candy.cs
@@ -23,7 +23,34 @@ namespace Leecodeoj
     {
         public int calculateMinimunCandies(int[] ratings)
         {
-            throw new NotImplementedException();
+            if (ratings == null || ratings.Length == 0)
+                return 0;
+            else if (ratings.Length == 1)
+                return 1;
+
+            int[] candies = new int[ratings.Length];
+            candies[0] = 1;
+
+            for (int i = 1; i < ratings.Length; i++)
+            {
+                if (ratings[i - 1] < ratings[i])
+                    candies[i] = candies[i - 1] + 1;
+                else
+                    candies[i] = 1;
+            }
+
+            for (int i = ratings.Length - 1; i > 0; i--)
+            {
+                if (ratings[i] < ratings[i - 1] && candies[i] >= candies[i - 1])
+                    candies[i - 1] = candies[i] + 1;
+            }
+
+            int minimunCandies = 0;
+            foreach (var item in candies)
+            {
+                minimunCandies += item;
+            }
+            return minimunCandies;
         }
     }
 }
