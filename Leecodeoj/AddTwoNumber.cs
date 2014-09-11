6b17544e2721bba0c297ba36ce243254f91697a4
pajace_chen <pajace_chen@htc.com>
2014-09-11 18:26:28 +0800

    add comment

--- Leecodeoj/AddTwoNumber.cs
+++ Leecodeoj/AddTwoNumber.cs
@@ -4,7 +4,14 @@
 // <Reference>https://oj.leetcode.com/problems/add-two-numbers/</reference>
 //----------------------------------------------------------------------
 
+// You are given two linked lists representing two non-negative numbers. 
+// The digits are stored in reverse order and each of their nodes contain a
+// single digit. Add the two numbers and return it as a linked list.
+//
+// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+// Output: 7 -> 0 -> 8
 
+using Leecodeoj.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,9 +19,18 @@ using System.Text;
 
 namespace Leecodeoj
 {
+    /// <summary>
+    /// Add Two Number
+    /// </summary>
     public class AddTwoNumber
     {
-        public Common.ListNode addTwoNumbers(Common.ListNode root1, Common.ListNode root2)
+        /// <summary>
+        /// AddTwoNumber
+        /// </summary>
+        /// <param name="root1"> non-negative numbers list node 1</param>
+        /// <param name="root2"> non-negative numbers list node 2</param>
+        /// <returns></returns>
+        public ListNode addTwoNumbers(Common.ListNode root1, Common.ListNode root2)
         {
             throw new NotImplementedException();
         }
