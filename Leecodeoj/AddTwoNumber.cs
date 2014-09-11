e81557709fd4636ebbb23d5a6976759c785f9154
pajace_chen <pajace_chen@htc.com>
2014-09-11 18:29:35 +0800

    solve the problem - AddTwoNumber

--- Leecodeoj/AddTwoNumber.cs
+++ Leecodeoj/AddTwoNumber.cs
@@ -30,9 +30,52 @@ namespace Leecodeoj
         /// <param name="root1"> non-negative numbers list node 1</param>
         /// <param name="root2"> non-negative numbers list node 2</param>
         /// <returns></returns>
-        public ListNode addTwoNumbers(Common.ListNode root1, Common.ListNode root2)
+        public ListNode addTwoNumbers(Common.ListNode l1, Common.ListNode l2)
         {
-            throw new NotImplementedException();
+            ListNode result = null;
+            ListNode headNode = null;
+            int? carryDigit = null;
+
+
+            while (l1 != null || l2 != null || carryDigit != null)
+            {
+
+                if (l1 == null && l2 == null)
+                {
+                    result.next = new ListNode((int)carryDigit);
+                    break;
+                }
+
+                int sum = ((l1 == null) ? 0 : l1.val) + ((l2 == null) ? 0 : l2.val);
+                int digitValue = sum % 10;
+
+                if (result == null && carryDigit == null)
+                {
+                    result = new ListNode(digitValue);
+                    headNode = result;
+                }
+                else if (carryDigit != null)
+                {
+                    int nodeValue = (digitValue + (int)carryDigit) % 10;
+                    result.next = new ListNode(nodeValue);
+                    result = result.next;
+                }
+                else
+                {
+                    result.next = new ListNode(digitValue);
+                    result = result.next;
+                }
+
+                if (carryDigit != null && digitValue + carryDigit >= 10)
+                    carryDigit = (digitValue + carryDigit) / 10;
+                else
+                    carryDigit = ((sum / 10 == 0) ? (int?)null : (sum / 10));
+
+                l1 = (l1 == null) ? null : l1.next;
+                l2 = (l2 == null) ? null : l2.next;
+            }
+
+            return headNode;
         }
     }
 }
