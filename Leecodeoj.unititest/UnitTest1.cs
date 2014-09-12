3bedd8560851b774402c758125c646d97b0da820
pajace_chen <pajace_chen@htc.com>
2014-09-12 10:07:22 +0800

    Add problem - Remove Duplicates from sorted List

---
+++ Leecodeoj.unititest/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leecodeoj.Common;
+
+namespace Leecodeoj.unititest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private void AssertDuplicateSortedList(ListNode node, ListNode expected)
+        {
+            DuplicateSortedList duplicateSortedList = new DuplicateSortedList();
+            ListNode actual = duplicateSortedList.removeDuplicates(node);
+
+            while (expected != null)
+            {
+                Assert.Equals(expected.val, actual.val);
+
+                actual = actual.next;
+                expected = expected.next;
+            }
+
+            Assert.IsNull(actual);
+        }
+
+        [TestMethod]
+        public void Duplicate_ThreeNode_OneDuplicate()
+        {
+            ListNode node = new ListNode(1);
+            node.next = new ListNode(1);
+            node.next.next = new ListNode(2);
+
+            ListNode expected = new ListNode(1);
+            expected.next = new ListNode(2);
+
+            AssertDuplicateSortedList(node, expected);
+        }
+
+        [TestMethod]
+        public void Duplicate_EmptyTree_NoDuplicate()
+        {
+            AssertDuplicateSortedList(null, null);
+        }
+
+        [TestMethod]
+        public void Duplicate_ThreeNode_TwoDuplicate()
+        {
+            ListNode node = new ListNode(1);
+            node.next = new ListNode(1);
+            node.next.next = new ListNode(1);
+
+            ListNode expected = new ListNode(1);
+
+            AssertDuplicateSortedList(node, expected);
+        }
+    }
+}
---
+++ Leecodeoj/DuplicateSortedList.cs
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------------------------------
+// <author>Pajace Chen</author>
+// <email>pajace@gmail.com</email>
+// <Reference>https://oj.leetcode.com/problems/remove-duplicates-from-sorted-list/</reference>
+//----------------------------------------------------------------------
+
+//  Given a sorted linked list, delete all duplicates such that each element appear only once.
+//  
+//  For example,
+//  Given 1->1->2, return 1->2.
+//  Given 1->1->2->3->3, return 1->2->3.
+
+
+using System;
+namespace Leecodeoj
+{
+    /// <summary>
+    /// Remove Duplicates from Sorted List
+    /// </summary>
+    public class DuplicateSortedList
+    {
+        /// <summary>
+        /// Remove Duplicates from Sorted List
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public Common.ListNode removeDuplicates(Common.ListNode head)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
