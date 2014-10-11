2b290c6d5f7d8cd56b819e84c1fc3fe90af1dab6
pajace_chen <pajace_chen@htc.com>
2014-10-11 22:14:16 +0800

    refactor

--- Leecodeoj/RemoveElementUtil.cs
+++ Leecodeoj/RemoveElementUtil.cs
@@ -9,26 +9,16 @@ namespace Leecodeoj
     {
         public int removeElement(int[] data, int elem)
         {
-            if (data.Length == 1)
-            {
-                if (data[0] == elem)
-                    return 0;
-                return 1;
-            }
-
             int index = 0;
             for (int i = 0; i < data.Length; i++)
             {
-                if (data[index] == elem)
+                if (data[index] == elem && data[i] != elem)
                 {
-                    if (data[i] != elem)
-                    {
-                        data[index] = data[i];
-                        index++;
-                        data[i] = elem;
-                    }
+                    data[index] = data[i];
+                    index++;
+                    data[i] = elem;
                 }
-                else
+                else if (data[index] != elem && data[i] != elem)
                 {
                     index++;
                 }
