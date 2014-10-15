6e069c4caac32ff40b219dce53690e6ba6ae1904
pajace_chen <pajace_chen@htc.com>
2014-10-15 14:13:19 +0800

    新增題目 Maximun Product Subarray

---
+++ Leecodeoj.unititest/MaxProductSubarray_unittest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leecodeoj;
+
+namespace Leecodeoj.unititest
+{
+    [TestClass]
+    public class MaxProductSubarray_unittest
+    {
+        private void assertMaxProduct(int[] inputArray, int expectedMaxProduct)
+        {
+            MaxProductSubarray maxProductSubArray = new MaxProductSubarray();
+            int actualMaxProduct = maxProductSubArray.FindMaxProduct(inputArray);
+
+            Assert.AreEqual(expectedMaxProduct, actualMaxProduct);
+        }
+
+        [TestMethod]
+        public void MaxProductSubarray_2element_product6()
+        {
+            int[] inputArray = { 2, 3, -4, 4};
+            int expectedMaxProduct = 6;
+
+            assertMaxProduct(inputArray, expectedMaxProduct);
+        }
+    }
+}
---
+++ Leecodeoj/MaxProductSubarray.cs
@@ -0,0 +1,30 @@
+﻿//-----------------------------------------------------------------------
+// <author>Pajace Chen</author>
+// <email>pajace@gmail.com</email>
+// <Reference> https://oj.leetcode.com/problems/maximum-product-subarray/ </reference>
+//----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Leecodeoj
+{
+    public class MaxProductSubarray
+    {
+        public int FindMaxProduct(int[] array)
+        {
+            if (array.Length == 1)
+                return array[0];
+
+            int maxProduct = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                int product = array[i - 1] * array[i];
+                if (product > maxProduct)
+                    maxProduct = product;
+            }
+            return maxProduct;
+        }
+    }
+}
