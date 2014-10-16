0632685b5f0b49534318fa852dd92d3438ff9ebe
pajace_chen <pajace_chen@htc.com>
2014-10-16 17:13:06 +0800

    solve count and say

---
+++ Leecodeoj.unititest/CountAndSay_Unittest.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leecodeoj;
+
+namespace Leecodeoj.unititest
+{
+    [TestClass]
+    public class CountAndSay_Unittest
+    {
+        private void AssertCountAndSay(int inputNumber, String expectedSay)
+        {
+            CountAndSay c = new CountAndSay();
+            string actual = c.SayIt(inputNumber);
+
+            Assert.AreEqual(expectedSay, actual);
+        }
+
+        [TestMethod]
+        public void CountAndSay_Input1()
+        {
+            int inputNumber = 1;
+            string expectd = "1";
+
+            AssertCountAndSay(inputNumber, expectd);
+        }
+
+        [TestMethod]
+        public void CountAndSay_Input2()
+        {
+            int inputNumber = 2;
+            string expectd = "11";
+
+            AssertCountAndSay(inputNumber, expectd);
+        }
+
+        [TestMethod]
+        public void CountAndSay_Input3()
+        {
+            int inputNumber = 3;
+            string expectd = "21";
+
+            AssertCountAndSay(inputNumber, expectd);
+        }
+
+        [TestMethod]
+        public void CountAndSay_Input4()
+        {
+            int inputNumber = 4;
+            string expectd = "1211";
+
+            AssertCountAndSay(inputNumber, expectd);
+        }
+
+        [TestMethod]
+        public void CountAndSay_Input5()
+        {
+            int inputNumber = 5;
+            string expectd = "111221";
+
+            AssertCountAndSay(inputNumber, expectd);
+        }
+
+        [TestMethod]
+        public void CountAndSay_Input6()
+        {
+            int inputNumber = 6;
+            string expectd = "312211";
+
+            AssertCountAndSay(inputNumber, expectd);
+        }
+
+        [TestMethod]
+        public void CountAndSay_Input7()
+        {
+            int inputNumber = 7;
+            string expectd = "13112221";
+
+            AssertCountAndSay(inputNumber, expectd);
+        }
+
+        [TestMethod]
+        public void CountAndSay_Input8()
+        {
+            int inputNumber = 8;
+            string expectd = "1113213211";
+
+            AssertCountAndSay(inputNumber, expectd);
+        }
+
+        [TestMethod]
+        public void CountAndSay_Input9()
+        {
+            int inputNumber = 9;
+            string expectd = "31131211131221";
+
+            AssertCountAndSay(inputNumber, expectd);
+        }
+
+        [TestMethod]
+        public void CountAndSay_Input10()
+        {
+            int inputNumber = 10;
+            string expectd = "13211311123113112211";
+
+            AssertCountAndSay(inputNumber, expectd);
+        }
+
+        [TestMethod]
+        public void CountAndSay_Input11()
+        {
+            int inputNumber = 11;
+            string expectd = "11131221133112132113212221";
+
+            AssertCountAndSay(inputNumber, expectd);
+        }
+    }
+}
---
+++ Leecodeoj/CountAndSay.cs
@@ -0,0 +1,53 @@
+﻿//-----------------------------------------------------------------------
+// <author>Pajace Chen</author>
+// <email>pajace@gmail.com</email>
+// <Reference> https://oj.leetcode.com/problems/count-and-say/ </reference>
+//----------------------------------------------------------------------
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Leecodeoj
+{
+    public class CountAndSay
+    {
+        public string SayIt(int inputNumber)
+        {
+            string result = "1";
+            for (int i = 0; i < inputNumber-1; i++)
+                result = calculateNextSequence(result);
+
+            return result;
+        }
+
+        private string calculateNextSequence(string inputString)
+        {
+            StringBuilder builder = new StringBuilder();
+            
+            char number = inputString[0];
+            int count = 1;
+            for (int i = 1; i < inputString.Count(); i++)
+            {
+                if (number == inputString[i])
+                    count += 1;
+                else
+                {
+                    builder.Append(count.ToString());
+                    builder.Append(number);
+
+                    count = 1;
+                    number = inputString[i];
+                }
+            }
+
+            builder.Append(count.ToString());
+            builder.Append(number);
+
+            return builder.ToString();
+        }
+    }
+}
