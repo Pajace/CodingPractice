47be0fb61b8c263f595f18d4e13b52c05e801fd8
pajace_chen <pajace_chen@htc.com>
2014-10-19 21:56:13 +0800

    Solve problem - palindrome

---
+++ Leecodeoj.unititest/PalindromeNumber_Unittest.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leecodeoj;
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
+
+namespace Leecodeoj.unititest
+{
+    [TestClass]
+    public class PalindromeNumber_Unittest
+    {
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public string GetInputNumberFromMethodName()
+        {
+            StackTrace st = new StackTrace();
+            StackFrame sf = st.GetFrame(1);
+            var methodName = sf.GetMethod().Name;
+            return methodName.Replace("PalindromNumber_TestCase_Input_", "").Replace("_", "-");
+        }
+
+
+        private void AssertPalindromeNumber(int inputNumber, bool expected)
+        {
+            PalindromeNumber palindrome = new PalindromeNumber();
+            bool actual = palindrome.IsPalindrome(inputNumber);
+
+            Assert.AreEqual(expected, actual, "InputNumber is " + inputNumber.ToString());
+        }
+
+        [TestMethod]
+        public void PalindromNumber_TestCase_Input_0()
+        {
+            int inputNumber = int.Parse(GetInputNumberFromMethodName());
+            bool expected = true;
+            AssertPalindromeNumber(inputNumber, expected);
+        }
+
+        [TestMethod]
+        public void PalindromNumber_TestCase_Input_1()
+        {
+            int inputNumber = int.Parse(GetInputNumberFromMethodName());
+            bool expected = true;
+            AssertPalindromeNumber(inputNumber, expected);
+        }
+
+        [TestMethod]
+        public void PalindromNumber_TestCase_Input_3()
+        {
+            int inputNumber = int.Parse(GetInputNumberFromMethodName());
+            bool expected = true;
+            AssertPalindromeNumber(inputNumber, expected);
+        }
+
+        [TestMethod]
+        public void PalindromNumber_TestCase_Input_5()
+        {
+            int inputNumber = int.Parse(GetInputNumberFromMethodName());
+            bool expected = true;
+            AssertPalindromeNumber(inputNumber, expected);
+        }
+
+        [TestMethod]
+        public void PalindromNumber_TestCase_Input_7()
+        {
+            int inputNumber = int.Parse(GetInputNumberFromMethodName());
+            bool expected = true;
+            AssertPalindromeNumber(inputNumber, expected);
+        }
+
+        [TestMethod]
+        public void PalindromNumber_TestCase_Input_9()
+        {
+            int inputNumber = int.Parse(GetInputNumberFromMethodName());
+            bool expected = true;
+            AssertPalindromeNumber(inputNumber, expected);
+        }
+
+        [TestMethod]
+        public void PalindromNumber_TestCase_Input__1()
+        {
+            int inputNumber = int.Parse(GetInputNumberFromMethodName());
+            bool expected = false;
+            AssertPalindromeNumber(inputNumber, expected);
+        }
+
+        [TestMethod]
+        public void PalindromNumber_TestCase_Input_10()
+        {
+            int inputNumber = int.Parse(GetInputNumberFromMethodName());
+            bool expected = false;
+            AssertPalindromeNumber(inputNumber, expected);
+        }
+
+        [TestMethod]
+        public void PalindromNumber_TestCase_Input_313()
+        {
+            int inputNumber = int.Parse(GetInputNumberFromMethodName());
+            bool expected = true;
+            AssertPalindromeNumber(inputNumber, expected);
+        }
+
+        [TestMethod]
+        public void PalindromNumber_TestCase_Input_1001()
+        {
+            int inputNumber = int.Parse(GetInputNumberFromMethodName());
+            bool expected = true;
+            AssertPalindromeNumber(inputNumber, expected);
+        }
+
+        [TestMethod]
+        public void PalindromNumber_TestCase_Input_1000021()
+        {
+            int inputNumber = int.Parse(GetInputNumberFromMethodName());
+            bool expected = false;
+            AssertPalindromeNumber(inputNumber, expected);
+        }
+    }
+}
---
+++ Leecodeoj/PalindromeNumber.cs
@@ -0,0 +1,44 @@
+﻿//-----------------------------------------------------------------------
+// <author>Pajace Chen</author>
+// <email>pajace@gmail.com</email>
+// <Reference>https://oj.leetcode.com/problems/palindrome-number/</reference>
+//----------------------------------------------------------------------
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Leecodeoj
+{
+    public class PalindromeNumber
+    {
+        public bool IsPalindrome(int inputNumber)
+        {
+            if (inputNumber < 0)
+                return false;
+
+            int digitCount = 0;
+            int temp = inputNumber;
+            while (temp > 0)
+            {
+                temp /= 10;
+                digitCount++;
+            }
+
+            int loop_times = digitCount / 2;
+
+            for (int i = 0; i < loop_times; i++)
+            {
+                int highDigit = (inputNumber / (int)Math.Pow(10, digitCount - (i + 1))) % 10;
+                int lowDigit = (inputNumber % (int)Math.Pow(10, i + 1)) / (int)Math.Pow(10, i);
+
+                if (highDigit != lowDigit)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
